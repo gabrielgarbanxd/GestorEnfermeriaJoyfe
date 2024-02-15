295c29be58f8796414881a07295081b9d7b3db9a
Andres Salinas <andresalinasrodas@gmail.com>
2024-02-15 03:00:31 +0100

    Correccion Calendar.cs

--- GestorEnfermeriaJoyfe/Domain/Calendar/Calendar.cs
+++ GestorEnfermeriaJoyfe/Domain/Calendar/Calendar.cs
@@ -5,23 +5,23 @@ namespace GestorEnfermeriaJoyfe.Domain.Calendar
 {
     public class Calendar
     {
-        public int Id { get; private set; }
+        public CalendarId Id { get; private set; }
         public CalendarFecha Fecha { get; private set; }
-        public string Tarea { get; private set; }
+        public CalendarTarea Tarea { get; private set; }
 
-        public Calendar(int id, CalendarFecha fecha, string tarea)
+        public Calendar(CalendarId id, CalendarFecha fecha, CalendarTarea tarea)
         {
             Id = id;
             Fecha = fecha;
             Tarea = tarea;
         }
 
-        public static Calendar Create(int id, CalendarFecha fecha, string tarea)
+        public static Calendar Create(CalendarId id, CalendarFecha fecha, CalendarTarea tarea)
         {
             return new Calendar(id, fecha, tarea);
         }
 
-        public static Calendar FromPrimitives(int id, CalendarFecha fecha, string tarea)
+        public static Calendar FromPrimitives(CalendarId id, CalendarFecha fecha, CalendarTarea tarea)
         {
             return new Calendar(id, fecha, tarea);
         }
