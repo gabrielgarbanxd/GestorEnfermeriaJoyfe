d4eb36cc49cb66ce18215986d88639944a52e64d
Andres Salinas <andresalinasrodas@gmail.com>
2024-02-26 18:19:12 +0100

    CalendarAdapter, CalendarApp y MySQL

---
+++ GestorEnfermeriaJoyfe/ApplicationLayer/CalendarApp/TaskDeleter.cs
@@ -0,0 +1,22 @@
+﻿using GestorEnfermeriaJoyfe.Domain.Calendar;
+using GestorEnfermeriaJoyfe.Domain.Calendar.ValueObjects;
+using GestorEnfermeriaJoyfe.Infraestructure.CalendarPersistence;
+using System.Threading.Tasks;
+
+namespace GestorEnfermeriaJoyfe.ApplicationLayer.CalendarApp
+{
+    public class TaskDeleter
+    {
+        private readonly ICalendarContract _calendarRepository;
+
+        public TaskDeleter(ICalendarContract calendarRepository)
+        {
+            _calendarRepository = calendarRepository;
+        }
+
+        public async Task<bool> Run(int id)
+        {
+            return await _calendarRepository.DeleteAsync(id) > 0;
+        }
+    }
+}
---
+++ GestorEnfermeriaJoyfe/ApplicationLayer/CalendarApp/TaskUpdater.cs
@@ -0,0 +1,24 @@
+﻿using GestorEnfermeriaJoyfe.Domain.Calendar;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestorEnfermeriaJoyfe.ApplicationLayer.CalendarApp
+{
+    public class TaskUpdater
+    {
+        private readonly ICalendarContract _calendarRepository;
+
+        public TaskUpdater(ICalendarContract calendarRepository)
+        {
+            _calendarRepository = calendarRepository;
+        }
+
+        public async Task<bool> Run(Calendar calendarEntry)
+        {
+            return await _calendarRepository.UpdateAsync(calendarEntry) > 0;
+        }
+    }
+}
