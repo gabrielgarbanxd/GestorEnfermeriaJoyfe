f44aca8d780d9b30451b53e7bf640637d7715b32
Andres Salinas <andresalinasrodas@gmail.com>
2024-03-01 01:34:17 +0100

    Cites eliminado

--- GestorEnfermeriaJoyfe/Domain/Cites/ValueObjects/CitesId.cs
+++
@@ -1,9 +0,0 @@
-﻿using GestorEnfermeriaJoyfe.Domain.Shared;
-
-namespace GestorEnfermeriaJoyfe.Domain.Cites.ValueObjects
-{
-    public sealed class CitesId : IdValueObject
-    {
-        public CitesId(int value) : base(value) { }
-    }
-}
